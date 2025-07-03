f01783568785f63fbca72600702e8f92d7d8e798
SeongChoel <tldtlddl1234@gmail.com>
2025-07-03 21:29:55 +0900

    authTokenService and jjwt

---
+++ src/main/java/com/example/jwt/domain/member/member/service/AuthTokensService.java
@@ -0,0 +1,12 @@
+package com.example.jwt.domain.member.member.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthTokensService {
+
+
+}
