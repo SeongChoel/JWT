e4ed61df63fea7b33a1e42423a9ef41963029c41
SeongChoel <tldtlddl1234@gmail.com>
2025-07-03 22:08:55 +0900

    jwt util

--- src/test/java/com/example/jwt/AuthTokenServiceTest.java
+++ src/test/java/com/example/jwt/AuthTokenServiceTest.java
@@ -1,12 +1,17 @@
 package com.example.jwt;
 
 import com.example.jwt.domain.member.member.service.AuthTokensService;
+import com.example.jwt.standard.util.Ut;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Key;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +27,20 @@ public class AuthTokenServiceTest {
     @DisplayName("AuthTokenService 생성")
     void init() {
         assertThat(authTokensService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("jwt 생성")
+    void createToken() {
+        // 토큰 만료기간 : 1년
+        int expireSeconds = 60 * 60 * 24 * 365;
+        // 토큰 시크릿 키 -> 도장 찍는 룰
+        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+
+        String jwt = Ut.Jwt.createToken(secretKey,
+                expireSeconds,
+                Map.of("name","john","age",23));
+        assertThat(jwt).isNotBlank();
+        System.out.println("jwt = " + jwt);
     }
 }
