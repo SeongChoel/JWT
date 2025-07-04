f343b3db77a9b5228b7bd715f186da386f91f6f4
SeongChoel <tldtlddl1234@gmail.com>
2025-07-04 13:08:54 +0900

    test case refactoring

--- src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
+++ src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
@@ -30,10 +30,10 @@ public class AuthTokenService {
     }
 
     Map<String, Object> getPayload(String token) {
+
+        if(!Ut.Jwt.isValidToken(keyString, token)) return null;
+
         Map<String, Object> payload = Ut.Jwt.getPayload(keyString, token);
-
-        if(payload == null) return null;
-
         Number idNo = (Number)payload.get("id");
         long id = idNo.longValue();
 
